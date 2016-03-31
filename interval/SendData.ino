435df8ee323652708ba6c726038afeb2db171913
Legokcihi Duckscallion <legokichi@gmail.com>
2016-03-31 17:34:44 +0900

    はるこ用

--- interval/SendData.ino
+++ interval/SendData.ino
@@ -1,25 +1,18 @@
 
 struct SendData1 {
-  uint8_t type[8] = {'D','A','T','A','_','1','\0','\0'}; // 64 bits = 8 bytes = ASCII コード で 8 文字分の接頭辞。データ種別とか書いとくと便利。analogRead(10) の温度センサの値を "THERMO" とかで表せる
-  uint32_t timeStamp;       // この値を得た時刻。
+  //uint32_t timeStamp;       // この値を得た時刻。
   uint16_t value;           // analogReadの値。analogReadは 0~1023 の値を返す = 2 bytes = 16 bytes
   uint8_t tab = '\n';       // データ区切り
 };
 
-struct SendData2 {
-  uint8_t type[8] = {'D','A','T','A','_','2','\0','\0'}; // 64 bits = 8 bytes = ASCII コード で 8 文字分の接頭辞。
-  uint32_t timeStamp;       // この値を得た時刻。
-  uint16_t value;           // analogReadの値。analogReadは 0~1023 の値を返す = 2 bytes = 16 bytes
-  uint8_t tab = '\n';       // データ区切り
-};
 
 // SendData 構造体の内訳の例
 
 //struct SendData {
-//  uint64_t type = {'D', 'A', 'T', 'A', '\0', '\0', '\0', '\0'};
 //  uint32_t timeStamp = 1495200048;
 //  uint16_t value = 24;
-//};
+//  uint8_t tab  = '\n';
+//}; 32+16+8 = 56bit = 7byte
 
 // uint64_t type
 // 0b 0100 0100 | 0100 0001 | 0000 0000 | 0100 0001
--- interval/setup.ino
+++ interval/setup.ino
@@ -1,20 +1,19 @@
-# define __BUFFER_SIZE__ 32 // 送信バッファの大きさ
+# define __BUFFER_SIZE__ 16 // 送信バッファの大きさ
 
 // 構造体をグローバル変数で使うよ宣言
 struct IntervalTimer timer1; // サンプリングレートを保証するための精密タイマ
-struct IntervalTimer timer2; // 頻度が異なる何らかの定期実行タスク用のタイマ
 struct SendData1 sendData1s[__BUFFER_SIZE__];
-struct SendData2 sendData2s[__BUFFER_SIZE__];
-uint32_t counter1 = 0;
-uint32_t counter2 = 0;
+uint32_t counter1 = 0; // __BUFFER_SIZE__ 回ごとに送信するためのカウンタ
+uint32_t prevLoop = 0; // 前回のフレーム
+uint8_t lag = 0;
+uint32_t buffer_length = sizeof(SendData1) * __BUFFER_SIZE__; // バイト配列の大きさを計算
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(3000); // ArduinoのICの電圧が安定するまでしばし待つ
   uint32_t startTime = micros(); // 基準となる時間
   // 初期化
-  initIntervalTimer(&timer1, startTime,   10000);
-  initIntervalTimer(&timer2, startTime,   50000);
+  initIntervalTimer(&timer1, startTime, 500); // 1/0.0005 = 2000Hz = 2KHz
   Serial.println(timer1.intervalTime);
 }
 
@@ -23,36 +22,18 @@ void loop() {
   
   if( stepIntervalTimer(&timer1, currentTime) ){ // サンプリングの時間
     uint32_t i = counter1 % __BUFFER_SIZE__;
-    sendData1s[i].timeStamp = currentTime;
+    //sendData1s[i].timeStamp = currentTime;
     sendData1s[i].value     = analogRead(A0);
+     lag += timer1.nthLoop - prevLoop - 1; // フレーム落ち検出
     if( i == __BUFFER_SIZE__ - 1 ){                       // バッファが一杯になった
-      uint32_t len = sizeof(SendData1) * __BUFFER_SIZE__; // バイト配列の大きさを計算
       uint8_t *buf = (uint8_t*)sendData1s;                // 構造体をバイト配列にキャスト
-      // Serial.write でメモリ上のバイト配列を送信した方が文字列に変換してから送るより通信効率は良い。
-      Serial.write(buf, len);
+      Serial.write(buf, buffer_length);
+      //Serial.println();
+      Serial.println(lag);
+      lag = 0;
     }
+    prevLoop = timer1.nthLoop;
     counter1++;
-  }
-  
-  if( stepIntervalTimer(&timer2, currentTime) ){ // サンプリングの時間
-    uint32_t i = counter2 % __BUFFER_SIZE__;
-    sendData2s[i].timeStamp = currentTime;
-    sendData2s[i].value     = analogRead(A1);
-    if( i == __BUFFER_SIZE__ - 1 ){                      // バッファが一杯になった
-      // Serial.print は文字列送信用。
-      // なので数値を送ろうとすると10進数のASCII文字列になる
-      // 送信情報量が増えて通信が遅くなるので数値送信には向いていない
-      // Serial.print はタブ(\t)区切りにすることでシリアルモニタで文字列を見やすくすることができる
-      for(uint32_t j=0; j<__BUFFER_SIZE__; j++){
-        Serial.print((char*)sendData2s[j].type);
-        Serial.print('\t');
-        Serial.print(sendData2s[j].timeStamp);
-        Serial.print('\t');
-        Serial.print(sendData2s[j].value);
-        Serial.print('\n'); // println で改行して送信
-      }
-    }
-    counter2++;
   }
 }
 
